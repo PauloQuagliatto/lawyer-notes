f689ff1f3ee2177ae7c6e2ff0a5af9e863238990
PauloQuagliatto <pquagliatto17@gmail.com>
2021-02-13 11:29:08 -0300

    removed unecessary things

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { startLogout } from '../actions/auth'
-import { wipeandReSetNotes } from '../actions/notes'
-import refreshImage from '../assets/images/reload-button.png'
 
 const Header = ({ startLogout, wipeandReSetNotes }) => (
   <header className="header">
@@ -12,23 +10,14 @@ const Header = ({ startLogout, wipeandReSetNotes }) => (
         <Link className="header__title" to="/dashboard">
           <h1>Notas do Advogado</h1>
         </Link>
-        <div className="header__buttons">
-          <button className="btn__refresh"
-            style={{
-              background: `url(${refreshImage})`,
-              backgroundSize: 'cover'
-            }} onClick={wipeandReSetNotes}>
-          </button>
-          <button className="btn btn--link" onClick={startLogout}>Logout</button>
-        </div>
+        <button className="btn btn--link" onClick={startLogout}>Logout</button>
       </div>
     </div>
   </header>
 )
 
 const mapDispatchToProps = (dispatch) => ({
-  startLogout: dispatch(startLogout),
-  wipeandReSetNotes: dispatch(wipeandReSetNotes)
+  startLogout: dispatch(startLogout)
 })
 
 export default connect(undefined, mapDispatchToProps)(Header)
